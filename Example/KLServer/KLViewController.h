edc8ae09bd0b024733832d16a4dfe2320d190859
Kalan <Kalanhall@163.com>
2019-07-18 09:55:42 +0800

    Initial commit

---
+++ Example/KLServer/KLViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLViewController.h
+//  KLServer
+//
+//  Created by Kalanhall@163.com on 07/18/2019.
+//  Copyright (c) 2019 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLViewController : UIViewController
+
+@end
---
+++ Example/KLServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KLServer
+//
+//  Created by Kalanhall@163.com on 07/18/2019.
+//  Copyright (c) 2019 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "KLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KLAppDelegate class]));
+    }
+}
